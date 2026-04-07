37184fbae15d5e008faed5e3bca813132ad33398
Manoharran Nadarr <manoharan.pandian@yahoo.in>
2026-04-07 12:56:13 +0530

    Create basic GUI without interactions, day 16

--- Excercises/chapter144.py
+++ Excercises/chapter144.py
@@ -1,6 +1,23 @@
 import random
 
-lower_bound = int(input("Enter lower bound of the range: "))
-upper_bound = int(input("Enter upper bound of the range: "))
+while True:
+    try:
+        lower_bound = input("Enter lower bound of the range or q to quit: ")
+        if not lower_bound == "q":
+            upper_bound = input("Enter upper bound of the range or q to quit: ")
 
-print(random.randint(lower_bound, upper_bound))
+            if not lower_bound == "q":
+                upper_bound = int(upper_bound)
+                lower_bound = int(lower_bound)
+
+                if lower_bound > upper_bound:
+                    print("The lower bound is greater than the upper bound and hence swapping them")
+                    lower_bound, upper_bound = upper_bound, lower_bound
+
+                print(random.randint(lower_bound, upper_bound))
+        else:
+            print("Bye!")
+            break
+    except ValueError:
+        print("You didn't enter a number.")
+        continue
