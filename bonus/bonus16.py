d6e74bd718c166768dc358cdfac3b29cc2023e1e
Manoharran Nadarr <manoharan.pandian@yahoo.in>
2026-04-08 13:32:57 +0530

    Implement the Complete and Exit Button

--- bonus/bonus16.py
+++ bonus/bonus16.py
@@ -1,19 +1,29 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
 
 label1 = sg.Text("Select files to compress:")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose files")
+choose_button1 = sg.FilesBrowse("Choose files", key="files")
 
 
 label2 = sg.Text("Select destination folder: ")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose folder")
+choose_button2 = sg.FolderBrowse("Choose folder", key="folder")
 
 compress_button = sg.Button("Compress Files")
+output_label = sg.Text(key="output", text_color="Dark Blue")
 
 window = sg.Window("File Compressor", layout=[[label1, input1, choose_button1],
                                               [label2, input2, choose_button2],
-                                              [compress_button]])
+                                              [compress_button, output_label]],)
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Compression Completed")
 
 window.read()
 window.close()
