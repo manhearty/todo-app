86c8956ccb4ba1ce7738545c7a6e7e01c85bccfa
Manoharran Nadarr <manoharan.pandian@yahoo.in>
2026-04-09 11:51:02 +0530

    Initial Commit

---
+++ web_app1/functions.py
@@ -0,0 +1,17 @@
+import os
+FILEPATH = "todos.txt"
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt','w') as file:
+        pass
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return a list of todos items"""
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos( todos_arg,filepath=FILEPATH ):
+    """Write the todos items to a text file"""
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
